0350c56ccfbf946756a137672714ec2b3cd28340
Dawid Zomerfeld <dawid.zomerfeld@optibuy.com>
2022-07-13 21:44:27 +0200

    Dodanie podstawowych komponentów

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LayoutComponent} from './layout/layout.component';
+import {MenuComponent} from './layout/menu/menu.component';
+import {ToolbarComponent} from './layout/toolbar/toolbar.component';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    MenuComponent,
+    ToolbarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
